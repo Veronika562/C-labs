11fc893bb9c28e08b4cb3a315bb9b6e6921e2715
Veronika <70834036+Veronika562@users.noreply.github.com>
2021-05-12 14:55:20 +0300

    Update test.c

--- lab7/test.c
+++ lab7/test.c
@@ -6,21 +6,15 @@
 
 int main() {
     _main();
-    FILE* file1 = fopen("text_files/data.txt", "r");
-    FILE* file2 = fopen("text_files/out.txt", "r");
+    FILE* file1 = fopen("text_files/data.txt", "br");
+    FILE* file2 = fopen("text_files/out.txt", "rb");
     assert(file1 != NULL);
     assert(file2 != NULL);
-    //char ch1, ch2;
-    char line1[100];
-    char line2[100];
-    while (feof(file1) == 0 && feof(file2) == 0) {
-//         ch1 = getc(file1);
-//         ch2 = getc(file2);
-        if (fscanf(file1, "%s", line1) && fscanf(file2, "%s", line2)) {
-			assert(strcmp(line1, line2) == 0);
-		}
-        assert(strcmp(line1, line2) == 0);
-        //assert(ch1 == ch2);
+    char ch1, ch2;
+    while (ch1 != EOF && ch2 != EOF) {
+        ch1 = getc(file1);
+        ch2 = getc(file2);
+        assert(ch1 == ch2);
     }
     fclose(file1);
     fclose(file2);
@@ -30,10 +24,10 @@ int main() {
     assert(file3 != NULL);
     assert(file4 != NULL);
     char ch3, ch4;
-    while (ch3 != EOF || ch4 != EOF) {
+    while (ch3 != EOF && ch4 != EOF) {
         ch3 = getc(file3);
         ch4 = getc(file4);
-        //assert(ch3 == ch4);
+        assert(ch3 == ch4);
     }
     fclose(file3);
     fclose(file4);
